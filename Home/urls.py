fb56f89a12c607f7e58260d0b1b9568acc1dc990
Niclas Pettersson <40391901+trollchild@users.noreply.github.com>
2021-12-27 22:24:14 +0200

    Start

--- Home/urls.py
+++ Home/urls.py
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include, re_path
 
-from django.conf.urls.static import static
+from django.urls import path
 from django.conf import settings
-import debug_toolbar
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
-
+from . import views
+from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Home.urls')),
-]
+    path('Page', views.Page, name='Page'),
+
+    ]
--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+import logging #this will be deleted when we fix all debugs (on debug = false)
+from django.http import HttpResponse, Http404, JsonResponse
+
+def Page(request):
+    context = {}
+    return render(request, '/Test.HTML', context)
+
 
 # Create your views here.
